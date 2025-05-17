b134912194c2b85bb092e87521c1913be9a3b788
John Prada <jhonprada@gmail.com>
2025-05-17 10:50:44 -0500

    feat: Agregado header e intro con logo y texto de bienvenida

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import PhotoGallery from "./components/PhotoGalleryComponent";
+import Header from "./components/Header";
+import Introduction from "./components/Introduction";
+import "./styles.css";
+
 function App() {
   return (
     <>
+      <Header />
+      <Introduction />
       <PhotoGallery />
     </>
   );
---
+++ src/components/Introduction.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Introduction = () => {
+  return (
+    <section className="introduction">
+      <div className="introduction-content">
+        <p>Bienvenidos a la Galería de Corruptos de Colombia, una herramienta digital esencial en la lucha por la transparencia y la rendición de cuentas. Aquí encontrará un directorio exhaustivo de funcionarios públicos señalados por actos de corrupción, junto con el seguimiento detallado y actualizado de sus respectivos casos.</p>
+        <p>Nuestra misión es empoderar a la ciudadanía con información clave para comprender y vigilar de cerca la realidad de la corrupción en el país.</p>
+      </div>
+    </section>
+  );
+};
+
+export default Introduction;
